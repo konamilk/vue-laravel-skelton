c5c40bb4d1b0a4ca0aab6c2e7d48716eed2c4fdf
konamilk <hina.no.atelier@gmail.com>
2020-06-18 16:17:27 +0900

    Initial Release

---
+++ vue-cli/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{ fromApi }}</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      fromApi: '読み込み中…'
+    }
+  },
+  mounted () {
+    const axios = require('axios')
+    axios
+      .get('/api/test')
+      .then((response) => {
+        this.fromApi = response.data.data
+        console.log(response)
+      })
+  }
+}
+</script>
